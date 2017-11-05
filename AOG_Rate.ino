e8ab33f3b286b039ea6009f855a5a8e1e389ead9
Wedge <warren_seely@live.com>
2017-11-04 21:43:23 -0700

    Update AOG_Rate.ino
    Added fencerow code activated through manual switches
    
    Program flow adjustment, elimination of extraneous code

--- AOG_Rate.ino
+++ AOG_Rate.ino
@@ -5,7 +5,7 @@
   //#define   PWM_PIN    11  //PB3  
   //#define WORKSW_PIN A4  //PD4
 
-  #define pressure_Pin A3
+  #define pressure_Pin A7
   #define valvePin 3
   #define valvedirection 4
   #define flowinPin 2 //bound to digital pin 2 and interrupt 0
@@ -19,7 +19,11 @@
   #define RELAY6_PIN 10
   #define RELAY7_PIN 11
   #define RELAY8_PIN 12  
-  
+  #define RelayPowerPin A5 //PC5
+  #define rtFenceRowOut A4 //PC4
+  #define ltFenceRowOut A3 //PC3
+  #define rtFenceRowIn A2
+  #define ltFenceRowIn A1
    
   //loop time variables in microseconds
   const unsigned int LOOP_TIME = 50; //20hz 
@@ -28,13 +32,13 @@
   unsigned int dT = 50000;
   unsigned int count = 0;
   unsigned int watchdogTimer = 0;
-  int flag = 0, rate_delay = 0;
+  int flag = 0, rate_delay = 0, flag1 = 0;
   
   //program flow
   bool isDataFound = false, isSettingFound = false;
   int header = 0, tempHeader = 0, temp;
 
-  byte relay = 0;
+  byte relay = 0, fencerows = 0;
   float rateSetPoint = 0, groundSpeed = 0, accumulatedVolume = 99, litersPerMinFlow = 0; //speed from AgOpenGPS is multiplied by 4
   float calibrationFactor = 38.0407754, adjust_time = 0;                                 //calibrationFactor is the rated pulses per unit of measure(in my case, Raven 60P flowmeter is 720 pulses per 10gal)
   float reference_pressure = 40, reference_Flow = 0.75708; //from chart                  //This means that for liters we have: 72 / 3.78541 = 19.02 pulses per liter
@@ -48,12 +52,11 @@
 
 void setup()
 {
-
-  pinMode(pressure_Pin, INPUT);
-  pinMode(valvePin, OUTPUT);
-  pinMode(valvedirection, OUTPUT);
-  pinMode(flowinPin, INPUT);
-  digitalWrite(flowinPin, HIGH);
+  pinMode(pressure_Pin, INPUT); //reads the pressure transducer
+  pinMode(valvePin, OUTPUT); //turns 3way valve on and off
+  pinMode(valvedirection, OUTPUT); //direction of the 3way valve
+  pinMode(flowinPin, INPUT); //reads teh flowmeter
+  digitalWrite(flowinPin, HIGH); //trips on a change
   
   pinMode(LED_PIN, OUTPUT); //configure LED for output
   pinMode(RELAY1_PIN, OUTPUT); //configure RELAY1 for output //Pin 5
@@ -62,8 +65,20 @@ void setup()
   pinMode(RELAY4_PIN, OUTPUT); //configure RELAY4 for output //Pin 8
   pinMode(RELAY5_PIN, OUTPUT); //configure RELAY5 for output //Pin 9
   pinMode(RELAY6_PIN, OUTPUT); //configure RELAY6 for output //Pin 10
-  pinMode(RELAY7_PIN, OUTPUT); //configure RELAY7 for output //Pin A4
- pinMode(RELAY8_PIN, OUTPUT); //configure RELAY8 for output //Pin A5
+  pinMode(RELAY7_PIN, OUTPUT); //configure RELAY7 for output //Pin 11
+  pinMode(RELAY8_PIN, OUTPUT); //configure RELAY8 for output //Pin 12
+  
+  digitalWrite((PORTD & B11100000), HIGH); //Set all Port D relay outputs to HIGH so relays are OFF. Pull LOW to activate valve relay
+  digitalWrite((PORTB & B11111), HIGH); // Set all Port B relay outputs to HIGH so relays are OFF. Pull LOW to activate valve relay
+
+  /*Relay boards are an active LOW so a main power control ensures power must be on for valves to actuate*/
+  pinMode(RelayPowerPin, OUTPUT); //main relay power control
+  digitalWrite(RelayPowerPin, HIGH); //ensure power relay is on AFTER setting valve relay pins HIGH so valves are OFF
+
+  pinMode(rtFenceRowOut, OUTPUT); //Fencerow out control
+  pinMode(ltFenceRowOut, OUTPUT);
+  pinMode(rtFenceRowIn, INPUT); //fencerow in controls
+  pinMode(ltFenceRowIn, INPUT);
   
   //set up communication  
   Serial.begin(38400); 
@@ -85,17 +100,21 @@ void loop()
     dT = currentTime - lastTime;
     lastTime = currentTime;
 
-    SetRelays(); //turn on off sections
-
-    if(!flag && (relay & 255) && (millis() - rate_delay >= 3000)) //IF we are not coming directly from an "all off" condition and at least one relay is on
+    fencerows = (digitalRead(rtFenceRowIn) << 1) | digitalRead(ltFenceRowIn); //get the state of A1 and A2
+    
+    SetRelays(); //turn on/off sections and fencerows  
+    
+    if((!flag && (relay & 255) && (millis() - rate_delay >= 3000)) || fencerows) //IF we are not coming directly from an "all off" condition and at least one relay is on OR a fencerow is on
     {
       litersPerMinFlow = PPMReader.get_ppm() / calibrationFactor; //get the liters per minute since last checked
       adjust_time = kp * abs(rateSetPoint - litersPerMinFlow); //time to adjust valve is (kp milliseconds) * (difference in target rate vs applied rate)
+      flag1 = 1; //this ensures we are not always writing to turn the valve off outside of the timed loop even after the valve has been shut off
     }
     else if (relay == 0) //all relays off
     {
       flag = 1; //set flag so we know we are coming from an "all off" condition
       adjust_time = 0; //no need for valve movement when all are off
+      //maybe put a 3way valve open condition here to allieviate pressure spike from all sections coming off
     }
     else
     {
@@ -168,8 +187,12 @@ void loop()
   
   
   
-  if((millis() - oldtime1)  >= adjust_time) //shut the valve off. may not be needed??
-    digitalWrite(valvePin, 0); //shut the valve off 
+  if(((millis() - oldtime1)  >= adjust_time) && (flag1 == 1)) //shut the valve off. may not be needed??
+  {
+    digitalWrite(valvePin, 0); //shut the valve off
+    flag1 = 0; //reset flag
+  }
+     
   
   
   
